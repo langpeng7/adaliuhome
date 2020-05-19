944f044934838db9b61f7c7b057a4497fc5bb6b8
lp <langpeng7@sina.com>
2020-05-19 17:04:47 +0800

    上传图片第一版

--- src/controller/eater.js
+++ src/controller/eater.js
@@ -1,8 +1,10 @@
 const { exec } = require('../db/mysql')
-
+const uuid = require('uuid');
+let  random  = require('../auto/randomString')
+const fs = require('fs');
 const getList = () => {
     //let sql = `SELECT count FROM z_eater_person where id=1;`
-    let sql = `SELECT email FROM users where id=1;`
+    let sql = `SELECT email FROM consumerinfo where id=1;`
     return exec(sql)
 }
 
@@ -21,7 +23,61 @@ const updateCount = (eaterData) => {
     })
 }
 
+const addConsumer = (data) => {
+    console.log(data)
+    // id 就是要更新博客的 id
+    // eaterData 是一个博客对象，包含 title content 属性
+    // const countData = eaterData.count;
+    const uid = uuid.v1().toString();
+    const name = data.name;
+    const email = data.email;
+    const address = data.address;
+
+    const sql = `insert into consumerinfo(id,email,name,address)values ('${uid}','${email}','${name}','${address}')`;
+    // const sql = `update z_eater_person set count='${countData}' where id=1`
+    return exec(sql).then(data => {
+
+        if (data.affectedRows > 0) {
+            return exec(`SELECT * FROM consumerinfo;`)
+        }
+        return false
+    })
+}
+
+
+const savePic = (data) => {
+    console.log()
+    // id 就是要更新博客的 id
+    // eaterData 是一个博客对象，包含 title content 属性
+    // const countData = eaterData.count;
+
+    let  imgData = data.imgData;
+    let base64Data = imgData.replace(/^data:image\/\w+;base64,/, "")
+    let dataBuffer = new Buffer(base64Data, 'base64');
+    const picRandomName =  random.randomString(8)+new Date().getTime()
+
+    fs.writeFile('./public/headImage/'+picRandomName+".png", dataBuffer, function(err) {
+        if(err){
+            
+        }else{
+         
+        }
+    });
+
+
+    // const sql = `insert into consumerinfo(id,email,name,address)values ('${uid}','${email}','${name}','${address}')`;
+    // // const sql = `update z_eater_person set count='${countData}' where id=1`
+    // return exec(sql).then(data => {
+
+    //     if (data.affectedRows > 0) {
+    //         return exec(`SELECT * FROM consumerinfo;`)
+    //     }
+    //     return false
+    // })
+}
 module.exports = {
     getList,
     updateCount,
+    addConsumer,
+    savePic
 }
--- src/router/eater.js
+++ src/router/eater.js
@@ -1,6 +1,8 @@
 const {
     getList,
     updateCount,
+    addConsumer,
+    savePic,
 } = require('../controller/eater')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
@@ -16,7 +18,41 @@ const handleeaterRouter = (req, res) => {
             return new SuccessModel(listData)
         })
     }
- 
+     // 获取吃饭数量列表
+     if (method === 'POST' && req.path === '/electron/eat/add') {
+        console.log(req)
+        console.log("------------------------------------------------------------------------")
+        const result = addConsumer(req.body)
+        return result.then(val => {
+       
+            if (val) {
+                return new SuccessModel(
+                   {data:val}
+                )
+            } else {
+                return new ErrorModel('新增用户信息失败')
+            }
+            return new SuccessModel(val)
+        })
+    }
+
+    if (method === 'POST' && req.path === '/electron/eat/savePic') {
+
+        const result = savePic(req.body)
+        return result.then(val => {
+       
+            if (val) {
+                return new SuccessModel(
+                   {data:val}
+                )
+            } else {
+                return new ErrorModel('保存图片失败')
+            }
+            return new SuccessModel(val)
+        })
+    }
+
+
     if (method === 'POST' && req.path === '/electron/eat/update') {
         const result = updateCount(req.body)
         return result.then(val => {
